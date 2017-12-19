78d6629c02e059a87ee0232ea5fb2d0a7c01ad31
Miguel Silva <miguel.smsilva@gmail.com>
2017-12-19 11:07:36 +0000

    add blips - extend TechradarData to incorporate blips definition - create tooltip element that shows over hovered blips - replace dummy test points with real blips, position randomly inside the respective area

--- src/app.js
+++ src/app.js
@@ -5,16 +5,97 @@ import createTechradar from "./index";
 import type { TechradarData } from "./index";
 
 const data: TechradarData = {
-  rings: ["Adopt", "Trial", "Assess", "Hold"],
+  rings: [
+    { id: "adopt", name: "Adopt" },
+    { id: "trial", name: "Trial" },
+    { id: "assess", name: "Assess" },
+    { id: "hold", name: "Hold" },
+  ],
   slices: [
     {
       name: "Testing",
+      blipsByRing: {
+        adopt: [
+          {
+            name: "Jest",
+            url: "https://github.com/facebook/jest",
+            description: "Test Framework",
+          },
+          {
+            name: "Enzyme",
+            url: "https://github.com/airbnb/enzyme",
+            description: "React Testing Utilities",
+          },
+        ],
+        hold: [
+          {
+            name: "Mocha",
+            url: "https://github.com/mochajs/mocha",
+            description: "Test Framework",
+          },
+        ],
+      },
     },
     {
       name: "Frameworks & Ecosystems",
+      blipsByRing: {
+        adopt: [
+          {
+            name: "React",
+            url: "https://github.com/facebook/react",
+            description: "View library",
+          },
+        ],
+        assess: [
+          {
+            name: "Vue",
+            url: "https://github.com/vuejs/vue",
+            description: "View library",
+          },
+          {
+            name: "Angular (2+)",
+            url: "https://github.com/angular/angular",
+            description: "Framework",
+          },
+        ],
+        hold: [
+          {
+            name: "AngularJS (1)",
+            url: "https://github.com/angular/angular.js",
+            description: "Framework",
+          },
+          {
+            name: "jQuery",
+            url: "https://github.com/jquery/jquery",
+            description: "Ecosystem",
+          },
+        ],
+      },
     },
     {
       name: "App state-management",
+      blipsByRing: {
+        adopt: [
+          {
+            name: "Redux",
+            url: "https://github.com/reactjs/redux",
+            description: "State container with Time traveling",
+          },
+        ],
+        assess: [
+          {
+            name: "Mobx-State-Tree",
+            url: "https://github.com/mobxjs/mobx-state-tree",
+            description: "Mix of MobX with Redux",
+          },
+          {
+            name: "Statty",
+            url: "https://github.com/vesparny/statty",
+            description:
+              "React component based state management influenced by Redux",
+          },
+        ],
+      },
     },
   ],
 };
--- src/index.js
+++ src/index.js
@@ -8,26 +8,84 @@ import {
   scaleLog,
   scaleOrdinal,
   schemeCategory10,
+  event,
 } from "d3";
 
 const width = 600;
 const height = 600;
 const padding = 10;
+const blipRadius = 10;
+
+export type TechradarRing = {
+  id: string,
+  name: string,
+};
+
+export type TechradarBlip = {
+  name: string,
+  description?: string,
+  url?: string,
+};
 
 export type TechradarSlice = {
   name: string,
+  blipsByRing: {
+    [ringId: string]: TechradarBlip[],
+  },
 };
 
 export type TechradarData = {
   slices: TechradarSlice[],
-  rings: string[],
+  rings: TechradarRing[],
 };
 
 const createTechradar = (svgEl: any, data: TechradarData) => {
+  //create shared tooltip
+  const tooltip = select("body")
+    .append("div")
+    .style("font-size", "12px")
+    .style("color", "white")
+    .style("opacity", 0)
+    .style("position", "absolute")
+    .style("pointer-events", "none")
+    .style("border-radius", "3px")
+    .style("background-color", "black")
+    .style("padding", "5px 8px");
+
+  //add shared tooltip's text container
+  const tooltipSpan = tooltip.append("span");
+
+  //add tooltip's box arrow
+  tooltip
+    .append("div")
+    .text("▼")
+    .style("color", "black")
+    .style("position", "absolute")
+    .style("margin-top", "-3px")
+    .style("top", "100%")
+    .style("left", 0)
+    .style("width", "100%")
+    .style("text-align", "center");
+
+  const showTooltip = blip => {
+    tooltipSpan.text(blip.name);
+    tooltip
+      .style("opacity", 1)
+      .style("left", event.x - tooltip.node().offsetWidth / 2 + "px")
+      .style("top", event.y - tooltip.node().offsetHeight - 10 + "px");
+  };
+
+  const hideTooltip = () => {
+    tooltip.style("opacity", 0);
+    tooltipSpan.text("");
+  };
+
+  //setup base svg
   const techradar = select(svgEl)
     .attr("width", width)
     .attr("height", height);
 
+  //add centered container
   const container = techradar
     .append("g")
     .attr("transform", `translate(${width / 2}, ${height / 2})`);
@@ -46,70 +104,111 @@ const createTechradar = (svgEl: any, data: TechradarData) => {
 
   const arcs = pie().value(() => 1)(data.slices);
 
-  const ringPathInfoList = data.rings.map((ring, ringIndex) => {
+  //generate ring path info from data.rings
+  const ringPathInfoList = data.rings.map((ringData, ringIndex) => {
     const innerRadius = radiusScale(ringIndex + 1);
     const outerRadius = radiusScale(ringIndex + 2);
+
+    //arc path generator
     const generator = arc()
       .innerRadius(innerRadius)
       .outerRadius(outerRadius);
     return {
-      innerRadius,
-      outerRadius,
+      blipDistanceScale: scaleLinear().range([
+        innerRadius + blipRadius,
+        outerRadius - blipRadius,
+      ]),
       generator,
     };
   });
 
-  const areas = data.slices.reduce((acc, slice, sliceIndex) => {
-    const arc = arcs[sliceIndex];
+  //generate viewData ({blips, areas}) from combining data.slices with data.rings
+  const viewData = data.slices.reduce(
+    (acc, sliceData, sliceIndex) => {
+      const arc = arcs[sliceIndex];
 
-    //TEST: middle point angle
-    const angle = arc.startAngle + (arc.endAngle - arc.startAngle) / 2;
+      const blipAngleScale = scaleLinear().range([
+        arc.startAngle,
+        arc.endAngle,
+      ]);
 
-    const sliceAreas = data.rings.map((ring, ringIndex) => {
-      const ringPathInfo = ringPathInfoList[ringIndex];
+      const sliceViewData = data.rings.reduce(
+        (acc, ringData, ringIndex) => {
+          const ringPathInfo = ringPathInfoList[ringIndex];
 
-      //TEST: middle point distance
-      const distance =
-        ringPathInfo.innerRadius +
-        (ringPathInfo.outerRadius - ringPathInfo.innerRadius) / 2;
+          const blipDataList = sliceData.blipsByRing[ringData.id];
 
-      //TEST: middle point coords
-      const point = {
-        x: distance * Math.cos(angle - Math.PI / 2),
-        y: distance * Math.sin(angle - Math.PI / 2),
-      };
+          const blips = blipDataList
+            ? acc.blips.concat(
+                blipDataList.map(blip => {
+                  const distance = ringPathInfo.blipDistanceScale(
+                    Math.random()
+                  );
+                  const angle = blipAngleScale(Math.random());
+
+                  return {
+                    ...blip,
+                    sliceIndex,
+                    ringIndex,
+                    x: distance * Math.cos(angle - Math.PI / 2),
+                    y: distance * Math.sin(angle - Math.PI / 2),
+                  };
+                })
+              )
+            : acc.blips;
+
+          const area = {
+            sliceIndex,
+            ringIndex,
+            path: ringPathInfo.generator(arc),
+          };
+
+          return {
+            blips,
+            areas: acc.areas.concat(area),
+          };
+        },
+        {
+          areas: [],
+          blips: [],
+        }
+      );
 
       return {
-        sliceIndex,
-        ringIndex,
-        point,
-        path: ringPathInfo.generator(arc),
+        areas: acc.areas.concat(sliceViewData.areas),
+        blips: acc.blips.concat(sliceViewData.blips),
       };
-    });
-
-    return acc.concat(sliceAreas);
-  }, []);
+    },
+    {
+      areas: [],
+      blips: [],
+    }
+  );
 
   //add areas
   container
     .selectAll("path")
-    .data(areas)
+    .data(viewData.areas)
     .enter()
     .append("path")
-    .attr("fill", d => ringColorScale(d.ringIndex))
+    .attr("fill", area => ringColorScale(area.ringIndex))
     .attr("stroke", "black")
-    .attr("d", d => d.path);
+    .attr("d", area => area.path);
 
-  //TEST: add middle points
+  //add blips
   container
-    .selectAll("circle")
-    .data(areas)
+    .selectAll("g")
+    .data(viewData.blips)
     .enter()
+    .append("g")
+    .attr("transform", blip => `translate(${blip.x}, ${blip.y})`)
     .append("circle")
-    .attr("r", 10)
-    .attr("fill", d => sliceColorScale(d.sliceIndex))
-    .attr("cx", d => d.point.x)
-    .attr("cy", d => d.point.y);
+    .attr("r", blipRadius)
+    .attr("fill", blip => sliceColorScale(blip.sliceIndex))
+    .on("touchstart", showTooltip)
+    .on("mousemove", showTooltip)
+    .on("touchend", hideTooltip)
+    .on("mouseout", hideTooltip);
 
   return techradar;
 };
