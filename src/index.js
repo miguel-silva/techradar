eef98dab3d7c792b59307d2297e48dfcf2f516d3
Miguel Silva <miguel.smsilva@gmail.com>
2017-12-21 15:33:10 +0000

    try to generate non-overlapping blips

--- src/index.js
+++ src/index.js
@@ -87,6 +87,8 @@ const createTechradar = (targetEl: any, data: TechradarData) => {
     };
   });
 
+  const minBlipCenterDistance = blipRadius * 2;
+
   //generate viewData ({blips, areas}) from combining data.slices with data.rings
   const viewData = data.slices.reduce(
     (acc, sliceData, sliceIndex) => {
@@ -97,45 +99,6 @@ const createTechradar = (targetEl: any, data: TechradarData) => {
         (acc, ringData, ringIndex) => {
           const ringPathInfo = ringPathInfoList[ringIndex];
 
-          const blipDataList = sliceData.blipsByRing[ringData.id];
-
-          //generate blips, if there are any for the current slice's ring
-          const blips = blipDataList
-            ? acc.blips.concat(
-                blipDataList.map(blip => {
-                  //calculate random distance within the current ring
-                  const distance = ringPathInfo.blipDistanceScale(
-                    Math.random()
-                  );
-
-                  //calculate angle padding to avoid blip overlapping adjacent slices
-                  const anglePadding = Math.asin(blipRadius / distance);
-
-                  const minAngle = arc.startAngle + anglePadding;
-                  const maxAngle = arc.endAngle - anglePadding;
-
-                  const blipAngleScale = scaleLinear().range([
-                    minAngle,
-                    maxAngle,
-                  ]);
-
-                  //if there is enough space, calculate random angle within the current ring, otherwise center it
-                  const angle =
-                    maxAngle > minAngle
-                      ? blipAngleScale(Math.random())
-                      : blipAngleScale(0.5);
-
-                  return {
-                    ...blip,
-                    sliceIndex,
-                    ringIndex,
-                    x: distance * Math.cos(angle - Math.PI / 2),
-                    y: distance * Math.sin(angle - Math.PI / 2),
-                  };
-                })
-              )
-            : acc.blips;
-
           //generate area that is the interception of current slice and ring
           const area = {
             sliceIndex,
@@ -143,10 +106,79 @@ const createTechradar = (targetEl: any, data: TechradarData) => {
             path: ringPathInfo.generator(arc),
           };
 
-          return {
-            blips,
-            areas: acc.areas.concat(area),
-          };
+          acc.areas.push(area);
+
+          const blipDataList = sliceData.blipsByRing[ringData.id];
+
+          //avoid generating blips for the current slice's ring, if there aren't any
+          if (!blipDataList) {
+            return acc;
+          }
+
+          //calculate blip positions
+          const positions = blipDataList.reduce(acc => {
+            //repetively try to generate a new random position that doesn't collide with other existing ones
+            for (let tries = 1; ; tries++) {
+              //calculate random distance within the current ring
+              const distance = ringPathInfo.blipDistanceScale(Math.random());
+
+              //calculate angle padding to avoid blip overlapping adjacent slices
+              const anglePadding = Math.asin(blipRadius / distance);
+
+              const minAngle = arc.startAngle + anglePadding;
+              const maxAngle = arc.endAngle - anglePadding;
+
+              const blipAngleScale = scaleLinear().range([minAngle, maxAngle]);
+
+              //if there is enough space, calculate random angle within the current ring, otherwise center it
+              const angle =
+                maxAngle > minAngle
+                  ? blipAngleScale(Math.random())
+                  : blipAngleScale(0.5);
+
+              //convert to polar coords
+              const x = distance * Math.cos(angle - Math.PI / 2);
+              const y = distance * Math.sin(angle - Math.PI / 2);
+
+              //if it hasn't hit the limit of tries and overlaps another blip, go for another try
+              if (
+                tries < 10 &&
+                acc.some(
+                  other =>
+                    Math.hypot(other.x - x, other.y - y) < minBlipCenterDistance
+                )
+              ) {
+                //go for another try since it collided with others
+                continue;
+              }
+
+              return acc.concat({
+                angle,
+                distance,
+                x,
+                y,
+              });
+            }
+
+            return acc;
+          }, []);
+
+          //generate and add new blips
+          acc.blips = acc.blips.concat(
+            blipDataList.map((blip, blipIndex) => {
+              const position = positions[blipIndex];
+
+              return {
+                ...blip,
+                sliceIndex,
+                ringIndex,
+                x: position.x,
+                y: position.y,
+              };
+            })
+          );
+
+          return acc;
         },
         {
           areas: [],
