98861505ee1625c3176dcdcc0d1ce1b44c888e95
Miguel Silva <miguel.smsilva@gmail.com>
2018-01-18 14:30:25 +0200

    add vizData rings tests

--- src/generateTechradarVizData.spec.js
+++ src/generateTechradarVizData.spec.js
@@ -10,8 +10,18 @@ describe("generateTechradarVizData", () => {
       vizData = generateTechradarVizData(exampleData);
     });
 
-    it("should return some visualization data", () => {
-      expect(vizData).toBeDefined();
+    it("rings should contain all ring input data", () => {
+      vizData.rings.forEach((vizRing, ringIndex) => {
+        expect(vizRing).toEqual(
+          expect.objectContaining(exampleData.rings[ringIndex])
+        );
+      });
+    });
+
+    it("rings should have a unique color", () => {
+      const colorSet = new Set(vizData.rings.map(vizRing => vizRing.color));
+
+      expect(colorSet.size).toBe(vizData.rings.length);
     });
   });
 });
