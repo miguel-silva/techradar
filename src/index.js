1eb189853cbbf770cb218ac9fe94c2297d652a06
Miguel Silva <miguel.smsilva@gmail.com>
2018-01-29 11:54:17 +0000

    pre publish cleanup

--- src/index.js
+++ src/index.js
@@ -1,22 +1,13 @@
 // @flow
 
 import createTechradar from "./createTechradar";
-import generateTechradarVizData from "./generateTechradarVizData";
 
 import type {
   TechradarData,
   TechradarOptions,
-  TechradarVizOptions,
   TechradarVizData,
 } from "./types";
 
 export default createTechradar;
 
-export { generateTechradarVizData };
-
-export type {
-  TechradarData,
-  TechradarOptions,
-  TechradarVizOptions,
-  TechradarVizData,
-};
+export type { TechradarData, TechradarOptions, TechradarVizData };
